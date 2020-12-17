b67c8dee7deeedafd1fb55a680d66637b179c5b0
Matt Land <mland@vectra.ai>
2020-12-17 12:26:00 -0600

    Fixes for new aws cli version, support multi-region, multi-account

--- variables.tf
+++ variables.tf
@@ -13,3 +13,13 @@ variable "docker_image_tag" {
   description = "This is the tag which will be used for the image that you created"
   default     = "latest"
 }
+
+variable "aws_profile" {
+  type = string
+  description = "AWS_PROFILE to set for bash commands. Should match the provider"
+}
+
+variable "aws_region" {
+  type = string
+  description = "AWS_DEFAULT_REGION to set for bash commands."
+}
