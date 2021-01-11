117c55c4b83c878c719987c7c850de5fbf5a6bf7
Matt Land <mland@vectra.ai>
2021-01-11 14:21:35 -0600

    CLD-1883 add tag into all images

--- bin/build.sh
+++ bin/build.sh
@@ -13,6 +13,8 @@ aws_ecr_repository_url_with_tag=$2
 which aws > /dev/null || { echo 'ERROR: aws-cli is not installed' ; exit 1; }
 # Check that docker is installed and running
 which docker > /dev/null && docker ps > /dev/null || { echo 'ERROR: docker is not running' ; exit 1; }
+# Check that git is installed
+which git > /dev/null || { echo 'ERROR: git is not installed' ; exit 1; }
 
 # Connect into aws
 aws ecr get-login-password \
@@ -23,12 +25,21 @@ aws ecr get-login-password \
 #$(aws ecr get-login-password) || { echo 'ERROR: aws ecr login failed' ; exit 1; }
 
 
+{
+   # set GIT_VERSION from tags
+   GIT_VERSION=$(git -C $build_folder describe --long)
+} || {
+  # set fallback on hash
+  GIT_VERSION="0-0-$(git -C $build_folder rev-parse --short HEAD)"
+} || {
+  GIT_VERSION="0-0-0"
+}
 
 # Some Useful Debug
-echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
+echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile with GIT_VERSION $GIT_VERSION"
 
 # Build image
-docker build -t $aws_ecr_repository_url_with_tag $build_folder
+docker build --build-arg GIT_VERSION=${GIT_VERSION} -t $aws_ecr_repository_url_with_tag $build_folder
 
 # Push image
 docker push $aws_ecr_repository_url_with_tag
